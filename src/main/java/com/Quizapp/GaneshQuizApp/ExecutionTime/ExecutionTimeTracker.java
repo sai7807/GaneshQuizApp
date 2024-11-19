7cc1a643d875e6e5103e381b88d1ec3f67d7688f
sai7807 <ganeshsai8355@gmail.com>
2024-11-18 23:06:34 -0500

    ganesh Changes

--- src/main/java/com/Quizapp/GaneshQuizApp/ExecutionTime/ExecutionTimeTracker.java
+++ src/main/java/com/Quizapp/GaneshQuizApp/ExecutionTime/ExecutionTimeTracker.java
@@ -1,16 +1,32 @@
+//package com.Quizapp.GaneshQuizApp.ExecutionTime;
+//
+//import org.aspectj.lang.annotation.Aspect;
+//
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.logging.Logger;
+//
+//@Aspect
+//@Component
+//public class ExecutionTimeTracker {
+//        Logger logger = (Logger) LoggerFactory.getLogger(ExecutionTimeTracker.class);
+//}
 package com.Quizapp.GaneshQuizApp.ExecutionTime;
 
 import org.aspectj.lang.annotation.Aspect;
-
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.logging.Logger;
 
 @Aspect
 @Component
 public class ExecutionTimeTracker {
-        Logger logger = (Logger) LoggerFactory.getLogger(ExecutionTimeTracker.class);
+        // Use the SLF4J Logger directly
+        private static final Logger logger = LoggerFactory.getLogger(ExecutionTimeTracker.class);
 
-
+        // You can now use `logger` to log messages
+        public void logExample() {
+                logger.info("Logging from ExecutionTimeTracker");
+        }
 }
